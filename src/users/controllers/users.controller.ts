dd07c982cfde4b26df85c372a4e8ee9a47a9838f
안지홍 <hongsam0102@gmail.com>
2024-04-13 02:25:14 +0900

    feat(users): id로 user 정보 조회 기능 추가

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 import { GetUser } from '../../libs/decorators/get-user.decorator';
 import { UserDto } from '../../libs/dtos/user.dto';
 import { JwtGuard } from '../../auth/guards/jwt.guard';
+import { GetUserByIdDto } from '../dtos/get-user-by-id.dto';
 
 @Controller('users')
 export class UsersController {
@@ -12,5 +13,14 @@ export class UsersController {
   @UseGuards(JwtGuard)
   async getUser(@GetUser() user: UserDto): Promise<UserDto> {
     return user;
+  }
+
+  @Get('ids')
+  @UseGuards(JwtGuard)
+  async getUserByIds(
+    @GetUser() user: UserDto,
+    @Query() params: GetUserByIdDto,
+  ): Promise<UserDto[]> {
+    return this.usersService.getUserByIds(user, params);
   }
 }
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,6 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/services/prisma.service';
+import { UserDto } from '../../libs/dtos/user.dto';
+import { GetUserByIdDto } from '../dtos/get-user-by-id.dto';
 
 @Injectable()
 export class UsersService {
-  constructor() {}
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getUserByIds(
+    user: UserDto,
+    { userIds }: GetUserByIdDto,
+  ): Promise<UserDto[]> {
+    const splitIds = userIds.split(',');
+
+    const users = await this.prismaService.users.findMany({
+      where: {
+        id: { in: splitIds.map((id) => +id) },
+      },
+      select: {
+        id: true,
+        nickname: true,
+        profileUrl: true,
+      },
+    });
+    return users;
+  }
 }
