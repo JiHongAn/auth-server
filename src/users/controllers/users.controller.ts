aa87b7ad45f15aedeec888a5951b5ba2519ee1db
안지홍 <hongsam0102@gmail.com>
2024-04-13 17:41:07 +0900

    feat(users): 다수 유저 조회 기능 추가

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Patch, Query, UseGuards } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 import { GetUser } from '../../libs/decorators/get-user.decorator';
 import { UserDto } from '../../libs/dtos/user.dto';
 import { JwtGuard } from '../../auth/guards/jwt.guard';
 import { GetUserByIdDto } from '../dtos/get-user-by-id.dto';
+import { SuccessDto } from '../../libs/dtos/success.dto';
+import { GetUserResponseDto } from '../dtos/get-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -11,8 +13,14 @@ export class UsersController {
 
   @Get()
   @UseGuards(JwtGuard)
-  async getUser(@GetUser() user: UserDto): Promise<UserDto> {
-    return user;
+  async getUser(@GetUser() user: UserDto): Promise<GetUserResponseDto> {
+    return this.usersService.getUser(user);
+  }
+
+  @Patch()
+  @UseGuards(JwtGuard)
+  async updateUser(@GetUser() user: UserDto): Promise<SuccessDto> {
+    return this.usersService.updateUser(user);
   }
 
   @Get('ids')
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -2,11 +2,38 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/services/prisma.service';
 import { UserDto } from '../../libs/dtos/user.dto';
 import { GetUserByIdDto } from '../dtos/get-user-by-id.dto';
+import { SuccessDto } from '../../libs/dtos/success.dto';
+import { GetUserResponseDto } from '../dtos/get-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
+  /**
+   * Get User
+   */
+  async getUser({ id }: UserDto): Promise<GetUserResponseDto> {
+    return this.prismaService.users.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        nickname: true,
+        profileUrl: true,
+        introduction: true,
+      },
+    });
+  }
+
+  /**
+   * Update User
+   */
+  async updateUser(user: UserDto): Promise<SuccessDto> {
+    return undefined;
+  }
+
+  /**
+   * Get User By Ids
+   */
   async getUserByIds(
     user: UserDto,
     { userIds }: GetUserByIdDto,
