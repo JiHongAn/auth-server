388fa750d1c07adabc51d548b40ad84ec1ae227e
안지홍 <hongsam0102@gmail.com>
2024-04-14 17:46:36 +0900

    feat(updateUser): 유저 업데이트 기능 추가

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Patch, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Query, UseGuards } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 import { GetUser } from '../../libs/decorators/get-user.decorator';
 import { UserDto } from '../../libs/dtos/user.dto';
 import { JwtGuard } from '../../auth/guards/jwt.guard';
-import { GetUserByIdDto } from '../dtos/get-user-by-id.dto';
 import { SuccessDto } from '../../libs/dtos/success.dto';
 import { GetUserResponseDto } from '../dtos/get-user.dto';
+import { UpdateUserDto } from '../dtos/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -19,13 +19,22 @@ export class UsersController {
 
   @Patch()
   @UseGuards(JwtGuard)
-  async updateUser(@GetUser() user: UserDto): Promise<SuccessDto> {
-    return this.usersService.updateUser(user);
+  async updateUser(
+    @GetUser() user: UserDto,
+    @Body() params: UpdateUserDto,
+  ): Promise<SuccessDto> {
+    return this.usersService.updateUser(user, params);
   }
 
   @Get('ids')
   @UseGuards(JwtGuard)
-  async getUserByIds(@Query() params: GetUserByIdDto): Promise<UserDto[]> {
-    return this.usersService.getUserByIds(params);
+  async getUserByIds(@Query('userIds') userIds: string): Promise<UserDto[]> {
+    return this.usersService.getUserByIds(userIds);
+  }
+
+  @Get('finds')
+  @UseGuards(JwtGuard)
+  async findUserByIds(@Query('query') query: string): Promise<UserDto[]> {
+    return this.usersService.findUserByIds(query);
   }
 }
